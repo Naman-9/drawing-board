f7f82cf49c6cd4ec10a4bd7fd3e34d256d5dd62d
Naman-9 <namancp7@gmail.com>
2023-10-14 23:50:55 +0530

    implemented Undo & Redo Feature

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -7,6 +7,8 @@ import { menuItemClick, actionItemClick } from '@/slice/menuSlice';
 const Board = () => {
     const disptach = useDispatch();
     const canvasRef = useRef(null);
+    const drawHistory = useRef([]);
+    const historyPointer = useRef(0);
     const shouldDraw = useRef(false);
     const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
     const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
@@ -18,12 +20,17 @@ const Board = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
-        if(actionMenuItem === MENU_ITEMS.DOWNLOAD) {
+        if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
             const URL = canvas.toDataURL();  // returns a data URL containing a representation of the image in the format specified 
             const anchor = document.createElement('a');
             anchor.href = URL;
             anchor.download = 'sketch.jpg';   // name of file
-            anchor.click(); 
+            anchor.click();
+        } else if (actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO) {
+            if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO) historyPointer.current -= 1;    // 
+            if (historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO) historyPointer.current += 1;    // 
+            const imageData = drawHistory.current[historyPointer.current];
+            context.putImageData(imageData, 0, 0);  // puts the image data on canvas starting from 0,0
         }
         // here "actionMenuItem" will create pblm as download value is set to download and on clkg 2nd time it won't work. tf, update to null required.
         disptach(actionItemClick(null));
@@ -88,6 +95,11 @@ const Board = () => {
         };
         const handleMouseup = (e) => {
             shouldDraw.current = false;
+
+            // Undo Feature -- onmouseUp > capture image data > push to the array > if undo clckd point to arry -1
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height); // start from 0,0 to entire canvas
+            drawHistory.current.push(imageData);
+            historyPointer.current = drawHistory.current.length - 1;         // cannot use pop() as we need to implement redo feature
         };
 
         canvas.addEventListener('mousedown', handleMouseDown);      // mouse clicked
