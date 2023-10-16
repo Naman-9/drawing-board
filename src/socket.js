821401aba3b9858827314c4b97ebaefa6f2a9cf2
Naman-9 <namancp7@gmail.com>
2023-10-16 08:50:17 +0530

    endpoint added

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:5000/");
+const URL = process.env.NODE_ENV === 'production' ? "https://sketchbook-server-cgcr.onrender.com" : "http://localhost:5000/";
+export const socket = io(URL);
 
